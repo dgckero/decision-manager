59dfdebf7e2705e3ff6017caae3fa28ffede5dd9
David Gómez <dgckero@gmail.com>
2020-05-08 18:32:21 +0200

    add bootstrap-toggle,catch DecsionEsception on GlobalExceptionHandlerController, update views

--- decision-web/src/main/java/com/dgc/dm/web/controller/GlobalExceptionHandlerController.java
+++ decision-web/src/main/java/com/dgc/dm/web/controller/GlobalExceptionHandlerController.java
@@ -4,6 +4,7 @@
 
 package com.dgc.dm.web.controller;
 
+import com.dgc.dm.core.exception.DecisionException;
 import com.dgc.dm.web.error.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -23,8 +24,8 @@ public class GlobalExceptionHandlerController {
      * @param message
      * @return error view
      */
-    private ModelAndView generateErrorModelAndView (String message) {
-        final ModelAndView errorModelAndView = new ModelAndView(CommonController.ERROR_VIEW);
+    private ModelAndView generateErrorModelAndView (final String message) {
+        ModelAndView errorModelAndView = new ModelAndView(CommonController.ERROR_VIEW);
         errorModelAndView.getModel().put(CommonController.MODEL_MESSAGE, message);
         return errorModelAndView;
     }
@@ -36,9 +37,22 @@ public class GlobalExceptionHandlerController {
      * @return error view
      */
     @ExceptionHandler(NullPointerException.class)
-    public ModelAndView handleNullPointerException (final Exception e) {
+    public ModelAndView handleNullPointerException (Exception e) {
         log.error("Handle NullPointer exception {}", e);
-        return this.generateErrorModelAndView("Ha ocurrido un error en el servidor, por favor póngase en contacto con el administrador");
+        return generateErrorModelAndView("Ha ocurrido un error en el servidor, por favor póngase en contacto con el administrador");
+    }
+
+    /**
+     * Handle DecisionException
+     *
+     * @param e
+     * @return error view
+     */
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(DecisionException.class)
+    public ModelAndView handleDecisionException (Exception e) {
+        log.error("handle DecisionException {}", e);
+        return generateErrorModelAndView(e.getMessage());
     }
 
     /**
@@ -49,9 +63,9 @@ public class GlobalExceptionHandlerController {
      */
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ModelAndView handleAllException (final Exception e) {
+    public ModelAndView handleAllException (Exception e) {
         log.error("handle internal server error exception {}", e);
-        return this.generateErrorModelAndView("Ha ocurrido un error en el servidor, por favor póngase en contacto con el administrador");
+        return generateErrorModelAndView("Ha ocurrido un error en el servidor, por favor póngase en contacto con el administrador");
     }
 
     /**
@@ -63,7 +77,7 @@ public class GlobalExceptionHandlerController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ModelAndView handleResourceNotFoundException ( ) {
         log.error("handle resource not found exception");
-        return this.generateErrorModelAndView("El recurso al que intenta acceder no existe");
+        return generateErrorModelAndView("El recurso al que intenta acceder no existe");
     }
 
     /**
@@ -74,8 +88,8 @@ public class GlobalExceptionHandlerController {
      */
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ModelAndView handle (final NoHandlerFoundException ex) {
+    public ModelAndView handle (NoHandlerFoundException ex) {
         log.error("handle page not found exception {}", ex.getMessage());
-        return this.generateErrorModelAndView("La página a la que intenta acceder no existe");
+        return generateErrorModelAndView("La página a la que intenta acceder no existe");
     }
 }
